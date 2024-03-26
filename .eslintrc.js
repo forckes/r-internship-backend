a91e2f6e8a091c8b27b969be3efd2d862d0fc013
forckes <forckes@proton.me>
2024-03-26 19:12:35 +0200

    MOD eslinterrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,10 @@ module.exports = {
 		sourceType: 'module'
 	},
 	plugins: ['@typescript-eslint/eslint-plugin'],
-	extends: ['plugin:@typescript-eslint/recommended'],
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended'
+	],
 	root: true,
 	env: {
 		node: true,
@@ -17,6 +20,8 @@ module.exports = {
 		'@typescript-eslint/interface-name-prefix': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off'
+		'@typescript-eslint/no-explicit-any': 'off',
+		'no-console': 1,
+		'prettier/prettier': 0
 	}
 }
