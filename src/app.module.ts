85d53b08c01e6519f57e9b6418c3f6bd129efe8f
forckes <forckes@proton.me>
2024-03-26 17:45:31 +0200

    install @nestjs/serve-static and app-root-path packages, add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import appRootPath from 'app-root-path'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: `${appRootPath}/uploads`,
+			serveRoot: '/uploads'
+		}),
+		ConfigModule.forRoot()
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
