9f18c7aad15f2c6b7291e500f50b5b23e997c96c
forckes <forckes@proton.me>
2024-03-27 22:21:38 +0200

    controller for task list

--- src/task-list/task-list.controller.ts
+++ src/task-list/task-list.controller.ts
@@ -1,7 +1,49 @@
-import { Controller } from '@nestjs/common';
-import { TaskListService } from './task-list.service';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { TaskListService } from './task-list.service'
+import { TaskListDto } from './task-list.dto'
 
-@Controller('task-list')
+@Controller('task-lists')
 export class TaskListController {
-  constructor(private readonly taskListService: TaskListService) {}
+	constructor(private readonly taskListService: TaskListService) {}
+
+	@UsePipes(new ValidationPipe())
+	@Get()
+	async getAll() {
+		return this.taskListService.getAll()
+	}
+
+	@Get(':id')
+	async getListById(@Param('id') id: string) {
+		return this.taskListService.getById(+id)
+	}
+
+	@HttpCode(200)
+	@Post()
+	async create(@Body() dto: TaskListDto) {
+		return this.taskListService.create(dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Put(':id')
+	async update(@Param('id') categoryId: string, @Body() dto: TaskListDto) {
+		return this.taskListService.update(+categoryId, dto)
+	}
+
+	@HttpCode(200)
+	@Delete(':id')
+	async delete(@Param('id') categoryId: string) {
+		return this.taskListService.delete(+categoryId)
+	}
 }
