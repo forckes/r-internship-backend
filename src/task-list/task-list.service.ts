3fa9455d0c755bdbf51467779ee8a51fd2ff0d86
forckes <forckes@proton.me>
2024-03-27 21:57:13 +0200

    write base crud funcs

--- src/task-list/task-list.service.ts
+++ src/task-list/task-list.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { returnTaskListObject } from './return-task-list.object'
+import { TaskListDto } from './task-list.dto'
 
 @Injectable()
-export class TaskListService {}
+export class TaskListService {
+	constructor(private prisma: PrismaService) {}
+
+	async getAll() {
+		return this.prisma.taskList.findMany({
+			orderBy: {
+				createdAt: 'desc'
+			},
+			select: returnTaskListObject
+		})
+	}
+
+	async getById(taskId: number) {
+		return this.prisma.taskList.findUnique({
+			where: { id: taskId }
+		})
+	}
+
+	async create(dto: TaskListDto) {
+		return this.prisma.taskList.create({
+			data: {
+				name: dto.name
+			}
+		})
+	}
+
+	async update(taskListId: number, dto: TaskListDto) {
+		const taskList = await this.prisma.taskList.findUnique({
+			where: { id: taskListId }
+		})
+
+		if (taskList) throw new NotFoundException('Task list not found')
+
+		return this.prisma.taskList.update({
+			where: { id: taskListId },
+			data: {
+				name: dto.name
+			}
+		})
+	}
+
+	async delete(taskListId: number) {
+		const taskList = await this.prisma.taskList.findUnique({
+			where: { id: taskListId }
+		})
+
+		if (taskList) throw new NotFoundException('Task list not found')
+
+		return this.prisma.taskList.delete({
+			where: {
+				id: taskListId
+			}
+		})
+	}
+}
